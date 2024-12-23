1621d70633a93b185f945e55bd6a40862b9ff1db
이이삭 <dlrltn1091@gmail.com>
2024-12-23 17:37:05 +0900

    .

---
+++ src/handlers/register.handler.js
@@ -0,0 +1,10 @@
+//유저 입장시 , 아이디 만들고 넣어줌. 나갈때 아이디 삭제함.
+const registerHandler = (io) => {
+    io.on('connection', (socket) => { //io.on을 하면 connection이벤트가 일어나기 전까지 대기.
+      
+
+      
+    })// socket.on은 하나의 대상만.
+}
+
+export default registerHandler
---
+++ src/routes/user-router.js
@@ -0,0 +1,106 @@
+import express from "express";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const userdata = [];
+const router = express.Router();
+
+// ** 회원가입 API **
+router.post("/sign-up", async (req, res) => {
+    try {
+
+      const { username, password } = req.body;
+        // 비밀번호를 암호화(bcrypt 사용)하여 저장.
+        const hashedPassword = await bcrypt.hash(password, 10);
+        userdata.push({ username, password: hashedPassword });
+        console.log("Userdata:", userdata);
+
+        return res.status(201).json({
+            message: "회원가입이 완료되었습니다",
+          });
+        } catch (error) {
+          console.error(error); 
+          return res.status(500).json({ errorMessage: "서버 에러" }); // 서버 에러 메시지 반환
+        }
+      });
+
+// ** 로그인 API **
+router.post("/sign-in", async (req, res) => {
+try {
+     const { username, password } = req.body;
+    // 유저 데이터에서 입력된 username으로 유저 검색
+    const user = userdata.find((user) => user.username === username);
+
+    if (!user) {
+      return res.status(404).json({ message: "유저를 찾을 수 없습니다" });
+    }
+
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordCorrect) {
+      return res.status(401).json({ message: "비밀번호가 일치하지 않습니다" });
+    }
+
+    // 비밀번호가 일치하면 JWT 생성
+    const token = jwt.sign(
+        {
+          username: user.username, // JWT 페이로드에 사용자 키 포함
+        },
+        process.env.SECRET_KEY, // 비밀 키를 사용하여 서명
+        { expiresIn: "1h" } // 토큰 유효 기간을 1시간으로 설정
+      );
+      // 성공 시 authorization 헤더에 토큰 추가
+      res.setHeader("authorization", `Bearer ${token}`);
+
+    return res.status(200).json({
+        message: "로그인 되었습니다",
+      });
+    } catch (error) {
+      console.error(error); // 에러를 콘솔에 출력
+      return res.status(500).json({ errorMessage: "서버 에러" }); // 서버 에러 메시지 반환
+    }
+  });
+
+
+  //토큰 확인겸 유저 정보 조회.
+  router.get("/users", async (req, res, naxt) => {
+     const { authorization } = req.headers;
+    try {
+
+        const [tokenType, token] = authorization.split(' ');
+
+        if (!token || tokenType !== 'Bearer') return res
+        .status(401)
+        .json({ errorMessage: '로그인부터 해주세요' });
+      // 토큰 검증
+        const decoded = jwt.verify(token, process.env.SECRET_KEY);
+        //여기서 decoded 안에있는 유저 찾아서 비교.
+        const user = userdata.find((user) => user.username === decoded.username);
+
+        if (!user) {
+          return res.status(404).json({ message: "유저를 찾을 수 없습니다" });
+        }
+
+      return res.status(200).json({
+        data: user,
+      });
+    } catch (error) {
+      console.error('JWT 검증 실패:', error.message);
+
+    // 에러 종류에 따른 적절한 메시지 반환
+    const isTokenExpired = error.name === 'TokenExpiredError'; // 토큰 만료 여부 확인
+    const errorMessage = isTokenExpired
+      ? '토큰이 만료되었습니다. 다시 로그인해주세요.' // 토큰 만료 메시지
+      : '토큰 검증 실패'; // 일반 검증 실패 메시지
+
+    // 검증 실패 응답
+    return res
+      .status(401)
+      .json({ message: errorMessage, error: error.message });
+    }
+  });
+
+  export default router;
