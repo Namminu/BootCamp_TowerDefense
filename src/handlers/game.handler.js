bbf2b25391d96ce5cb8082e1604bdff40986cda7
Namminu <mvpalsdn771@gmail.com>
2024-12-24 21:02:57 +0900

    gameOver 핸들러 임시 구현
    1. gameOver 핸들러
    - payload 로 userId, currentRound 전달
    - updateHighScore 를 통해 DB와 값 비교
    - 이후 업데이트 여부에 따라 return - message 전환
    
    2. updateHighScore
    - prisma 사용해 DB접근
    - 해당 유저의 현재 최고기록과 현재 기록을 비교
    - 갱신 시 DB 업데이트 / 아닐 시 return
    - 갱신 여부는 return - updated : true/false 로 판단
    
    3. 랭킹 조회 api
    - rank.router에서 처리
    - highScore 컬럼을 Round 로 데이터 가공 후 출력
    
    현재 일부 로직(게임 종료 시 기록 갱신 외 필요한 추가 동작) 및 검증 없는 상태. 추후 추가 예정

--- src/handlers/game.handler.js
+++ src/handlers/game.handler.js
@@ -1,6 +1,7 @@
 // import { clearStage, getStage, setStage } from "../models/stage.model.js";
 // import { getHightScore, setHightScore } from "../models/user.model.js";
 
+import { updateHighScore } from "../models/rank.model";
 
 export const gameStart = (uuid, payload) => {
   const { stages } = getGameAssets();
@@ -13,14 +14,19 @@ export const gameStart = (uuid, payload) => {
 };
 
 // Base의 Hp <= 0 일 시 호출되는 이벤트
-export const gameOver = (uuid, payload) => {
+export const gameOver = async (uuid, payload) => {
   const rounds = getStage(uuid);
   if (!rounds.length) return { status: 'fail', message: 'No Rounds Found for User' };
 
-  temp;
+  const userId = payload.userId;
+  const currentRound = payload.currentRound;
 
-  //DB - HighScores 에서 유저의 최고 기록 가져와서 비교
   //최고 기록보다 현재 기록이 높다면 DB 갱신
+  const result = await updateHighScore(userId, currentRound);
 
-  return { status: 'success', message: "게임 끝", score };
+  return {
+    status: 'success',
+    message: result.updated ? '최고 기록 갱신!' : '게임 오버',
+    score: result.currentHighScore
+  };
 }
