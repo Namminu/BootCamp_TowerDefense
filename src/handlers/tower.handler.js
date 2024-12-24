93b9b7dbe61addb2e82f97d729db9699f86c28c2
이이삭 <dlrltn1091@gmail.com>
2024-12-24 17:51:26 +0900

    update.tower.handler,model.js
    일단 모양만 추가. 오늘 약속 없었으면 끝났겠다.

--- src/handlers/tower.handler.js
+++ src/handlers/tower.handler.js
@@ -1,3 +1,50 @@
 // 여기선 유저의 타워를 확인하고, 골드를 확인하고, 몬스터 잡을때 그 타워의 거리를 확인함.
 // 여기서 유저 골드도 확인해야 할듯. 알맞게 설치가 되고 있는지.
 // 몬스터 잡을때 올리면 되고.
+import { getTower, setTower } from "../models/tower.model";
+
+export const createTower = (userId, payload) => {
+
+let currentTowers = getTower(userId);
+
+const width = 78; // 타워 너비
+const height = 150; // 타워 높이
+
+const newTowerX = payload.x;
+const newTowerY = payload.y;
+
+const isTooClose = currentTowers.some((tower) => {
+    const dx = tower.x - newTowerX;
+    const dy = tower.y - newTowerY;
+
+    const distance = Math.sqrt(dx * dx + dy * dy); // 유클리드 거리 계산
+    return distance < (width / 2 + height / 2); // 겹침 여부 확인
+});
+
+if (isTooClose) {
+    return { status: 'fail', message: "타워가 너무 가까워서 배치할 수 없습니다." };
+}
+
+//유저 돈.
+
+setTower(userId, payload.x, payload.y ,payload.range, payload.attackPower, payload.cost, payload.cooldown, payload.level);
+
+};
+
+export const sellingTower = (userId, payload) => {
+
+
+
+};
+
+export const upgradeTower = (userId, payload) => {
+
+
+
+};
+
+export const killMonsters = (userId, payload) => {
+
+
+
+};
---
+++ src/models/tower.model.js
@@ -0,0 +1,31 @@
+const towers = {};
+
+
+export const createTower = (uuid) => {
+    towers[uuid] = [];
+}
+
+//유저 스테이지 보기.
+export const getTower = (uuid) => {
+    return towers[uuid];
+}
+
+export const setTower = (uuid, x, y, range, attackPower, cost, cooldown, level) => { //여기서, 공속, 사거리, 그런걸 확인해야함.피버타입인걸 확인해야 겠는데.
+    return towers[uuid].push({ x, y, range, attackPower, cost, cooldown, level });
+}
+
+export const removeTower = (uuid, x, y) => {
+    const index = towers[uuid].findIndex((tower) => tower.x === x && tower.y === y );
+  if (index !== -1) {//-1이면 없는거니까 뭐..
+    return towers[uuid].splice(index, 1)[0];
+  }
+};
+
+
+export const upgradeTower = (uuid, x, y) => {
+    const index = towers[uuid].findIndex((tower) => tower.x === x && tower.y === y );
+  if (index !== -1) {//-1이면 없는거니까 뭐..
+    towers[uuid][index].level += 1;
+    return towers[uuid][index];
+  }
+};
