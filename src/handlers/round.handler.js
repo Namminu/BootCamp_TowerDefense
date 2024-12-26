a18f746799a0bd8d8252f810795670337ddf37cb
dwook <dwook1637@gmail.com>
2024-12-26 18:58:41 +0900

    feat: moveRound 수정

--- src/handlers/round.handler.js
+++ src/handlers/round.handler.js
@@ -1,9 +1,11 @@
 import { getGameAssets } from "../init/assets.js";
-import { getRoundInfo } from "../models/roundInfo.model.js";
+import { createRoundInfo, getRoundInfo } from "../models/roundInfo.model.js";
 import { getUserData, setUserRound } from "../models/userData.model.js";
 
 // sendEvent(11, payload : { currentRound, timestamp })
 export const moveRoundHandler = (userId, payload) => {
+    const { monster, unlock_monster } = getGameAssets();
+    
     // 라운드 검증. 유저의 현재 라운드와 currentRound 비교
     if (payload.currentRound) {
         const currentRound = getUserData(userId).round;
@@ -14,12 +16,12 @@ export const moveRoundHandler = (userId, payload) => {
 
     // 진행행시간 검증. 해당 라운드의 진행시간과 실제 진행시간의 오차 계산
     if (payload.timestamp) {
-        const roundStartTime = 0;
-        const roundClearTime = payload.timestamp;
+        const roundStartTime = getUserData.timestamp;   // 현재 라운드 시작 시간
+        const roundClearTime = payload.timestamp;       // 현재 라운드 종료 시간
         const elapsedTime = (roundClearTime - roundStartTime) / 1000;
-        const roundTime = 0;
+        const roundTime = getRoundInfo(payload.currentRound).time;
         if (elapsedTime < roundTime - 10 || elapsedTime > roundTime + 10) {
-            return { status: "fail", message: "currnetStage mismatch" };
+            return { status: "fail", message: "elapsedTime out of scope" };
         }
     } else return { status: "fail", message: "no timestamp for moveRound" };
 
@@ -27,17 +29,16 @@ export const moveRoundHandler = (userId, payload) => {
     const nextRound = payload.currentRound + 1;
 
     // 다음 라운드 정보를 담은 객체 생성
+    // 서버에 저장된 다음 라운드 정보가 있다면 바로 저장, 없다면 생성 후 저장
     const nextRoundInfo = getRoundInfo(nextRound);
+    if(!nextRoundInfo) nextRoundInfo = createRoundInfo(nextRound);
 
     // 유저의 현재 라운드 정보 업데이트
     setUserRound(userId, nextRound, payload.timestamp );
 
-    // monster.json 에셋에서 다음 라운드에 해금되는 id인 몬스터들 저장
-    const { monsters, unlock_monster } = getGameAssets();
-    const unlockMonsters = monsters.data.find(mon => (mon.round = nextRound)); //[{ id:2 }];
+    const unlockMonsters = null;
+    //unlockMonsters = unlock_monster.data.find(mon => (mon.round = nextRound));
 
-    // 유저의 다음 라운드 정보 업데이트 + 다음 라운드 몬스터 해제
-    //setRound(userId, nextRound, Date.now());
     return {
         handlerId: 11,
         status: "success",
