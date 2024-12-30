ebd6e3f521674a49a92c8602c8ca556d20e391cd
Namminu <mvpalsdn771@gmail.com>
2024-12-30 16:59:47 +0900

    feat : 로그인/회원가입 모달창 제작중

--- public/src/webpages/modals/registerAndloginModal.js
+++ public/src/webpages/modals/registerAndloginModal.js
@@ -1,6 +1,27 @@
 // 모달 HTML 파일 로드
-async function loadModal(modalFile) {
-    const response = await fetch(`htmls/modalHTMLs/${modalFile}`);
-    const modalHtml = await response.text();
-    document.getElementById('modalContainer').innerHTML = modalHtml;
+export async function loadModal(modalFile) {
+    try {
+        const response = await fetch(`htmls/modalHTMLs/${modalFile}.html`);
+        const modalHtml = await response.text();
+
+        const modalContainer = document.createElement('div');
+        modalContainer.innerHTML = modalHtml;
+        document.body.appendChild(modalContainer);
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+export function showLogInModal() {
+    const modal = document.getElementById('loginModal');
+
+    modal.style.display = 'flex';
+    modal.classList.add('show');
+}
+
+export function showRegisterModal() {
+    const modal = document.getElementById('registerModal');
+
+    modal.style.display = 'flex';
+    modal.classList.add('show');
 }
--- public/src/webpages/pages/mainPage.js
+++ public/src/webpages/pages/mainPage.js
@@ -1,12 +1,21 @@
+import { loadModal, showModal } from '../modals/registerAndloginModal.js';
+
 const BASE_URL = 'http://localhost:8080/api';
+
+// 모달창 초기 로드
+loadModal('registerModal');
+loadModal('loginModal');
+
 // 회원가입 버튼
 document.getElementById('registerButton').addEventListener('click', () => {
     console.log("회원가입 버튼 클릭");
+    showModal('registerModal');
 });
 
 // 로그인 버튼
 document.getElementById('loginButton').addEventListener('click', () => {
     console.log("로그인 버튼 클릭");
+    showModal('loginModal');
 });
 
 // 게임 시작 버튼
