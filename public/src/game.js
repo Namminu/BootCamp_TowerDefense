eb1b1f4f2c5578276f14100482c72779018c09c4
이이삭 <dlrltn1091@gmail.com>
2025-01-01 15:43:38 +0900

    Update game.js

--- public/src/game.js
+++ public/src/game.js
@@ -218,7 +218,6 @@ let isRoundExpired = false;
 
 async function gameLoop(frameTime) {
 	if (!isGameRun) {
-		console.log("작동 확인 4");
 		return;
 	}
 	// 캔버스 새로 그리기
@@ -535,12 +534,10 @@ async function gameLoop(frameTime) {
 
 export async function initGame(receivedUserData, getReset = false) {
 	if ((isInitGame && !getReset) || !receivedUserData) {
-		console.log("작동 확인 1");
 		return; // 이미 초기화된 경우 방지
 	}
 
 	if (getReset) {
-		console.log("작동 확인 2");
 		isInitGame = false;  // resetGame으로 강제 초기화
 	}
 	userData = receivedUserData;
@@ -558,7 +555,6 @@ export async function initGame(receivedUserData, getReset = false) {
 	//await initModal();
 
 	if (monsterPath.length === 0) {
-		console.log("작동 확인 6");
 		console.error('monsterPath is not defined');
 		return;
 	}
@@ -572,7 +568,6 @@ export async function initGame(receivedUserData, getReset = false) {
 	placeBase(); // 기지 배치
 	// 서버에 몬스터 스폰 주기와 타이밍 동기화 -> 라운드 정보를 가져와서 초기화해야함 -> 0으로 초기화된거 너무 짜친다다
 	queueEvent(13, { round: round, timestamp: Date.now() });
-	console.log("작동 확인 3");
 	gameLoop(); // 게임 루프 시작
 
 	await initModal(); // 게임오버 모달창 초기 로드
